d36052392e7e1fb221552fe7d7f36ce67d64cb80
Stark-program <chad.d.stark@hotmail.com>
2023-05-10 17:47:26 -0600

    Finish landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      overpass: ["PT Sans, sans-serif"],
+    },
   },
   plugins: [],
 } satisfies Config;
+
+// font-family: 'PT Sans', sans-serif;
