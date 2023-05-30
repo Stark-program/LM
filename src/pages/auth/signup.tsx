4268b1196fc30b5b98a4233401ddb81a50ccb92a
Stark-program <chad.d.stark@hotmail.com>
2023-05-29 22:16:27 -0600

    Change schema to include password and add name field to signup

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -12,6 +12,22 @@ export default function SignUp() {
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
                 <label
+                  htmlFor="name"
+                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your Name
+                </label>
+                <input
+                  type="name"
+                  name="name"
+                  id="name"
+                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 sm:text-sm"
+                  placeholder="James Bond"
+                  required={true}
+                />
+              </div>
+              <div>
+                <label
                   htmlFor="email"
                   className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
                 >
