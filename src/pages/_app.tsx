99512ae345b8a62d3ad31ebe891d18d52b679eac
Stark-program <chad.d.stark@hotmail.com>
2023-06-27 22:52:18 -0600

    Add some meta information

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import MobileHamburger from "~/components/mobile/HamburgerMenu";
 import Header from "~/components/Header";
 import { useState } from "react";
 import { api } from "~/utils/api";
+import Head from "next/head";
 
 import "~/styles/globals.css";
 
@@ -21,20 +22,42 @@ const MyApp: AppType<{ session: Session | null }> = ({
     setMobileMenu(!mobileMenu);
   };
   return (
-    <ParallaxProvider>
-      <SessionProvider session={session}>
-        <MobileHamburger mobileMenu={handleMobileMenu} />
-        {mobileMenu ? (
-          <MobileMenu
-            mobileMenu={handleMobileMenu}
-            mobileMenuShow={mobileMenu}
-          />
-        ) : (
-          <Header />
-        )}
-        <Component {...pageProps} />
-      </SessionProvider>
-    </ParallaxProvider>
+    <>
+      <Head>
+        <meta
+          name="theme-color"
+          content="#151719"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#151719"
+          media="(prefers-color-scheme: dark)"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="apple-mobile-web-app-title" content="On The Minute" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <ParallaxProvider>
+        <SessionProvider session={session}>
+          <MobileHamburger mobileMenu={handleMobileMenu} />
+          {mobileMenu ? (
+            <MobileMenu
+              mobileMenu={handleMobileMenu}
+              mobileMenuShow={mobileMenu}
+            />
+          ) : (
+            <Header />
+          )}
+          <Component {...pageProps} />
+        </SessionProvider>
+      </ParallaxProvider>
+    </>
   );
 };
 
