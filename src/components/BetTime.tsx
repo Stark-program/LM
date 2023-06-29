09f015147221ab6761b32cd41c0e7b102ae27f43
Stark-program <chad.d.stark@hotmail.com>
2023-06-28 22:58:44 -0600

    Testing confirm modal

--- src/components/BetTime.tsx
+++ src/components/BetTime.tsx
@@ -36,11 +36,9 @@ export default function BetTime({
         ...prevState,
         { userName: resData.name, timeslot: resData.timeslot },
       ]);
-      setConfirmBetState(false);
     }
     if (res.status === 200 && !resData.success) {
       // alert("You have already placed a bet for this game");
-      setConfirmBetState(false);
       setBetHasBeenPlaced(true);
     }
   };
@@ -50,10 +48,9 @@ export default function BetTime({
   };
 
   //----------------- WORK IN PROGRESS --------------------------------
-  // const handleConfirmBet = () => {
-  //   setConfirmBetState(true);
-
-  // };
+  const handleConfirmBet = () => {
+    setConfirmBetState(true);
+  };
 
   const handleDelete = async (time: string, gameId: string) => {
     const res = await axios.post("/api/deletebet", {
@@ -112,7 +109,6 @@ export default function BetTime({
                   className="w-full rounded bg-[#fd3594ff] p-2 font-overpass text-lg font-bold text-black hover:bg-[#85214f] sm:w-1/5"
                   onClick={() => {
                     void handleBet(time, session.data?.user.email, gameId);
-                    // else void handleBet();
                   }}
                 >
                   {betTime}
@@ -121,20 +117,21 @@ export default function BetTime({
 
               {/* 
               --------------------WORK IN PROGRESS. CONFIRM BUTTON NOT WORKING PROPERLY ------------------
+                */}
               {confirmBetState ? (
                 <ConfirmBet
                   confirmInfo={{
-                    betTime: betTime,
-                    handleConfirm: () =>
-                      void handleConfirmBet(
-                        time,
-                        session.data?.user.email,
-                        gameId
-                      ),
+                    //   betTime: betTime,
+                    //   handleConfirm: () =>
+                    //     void handleConfirmBet(
+                    //       time,
+                    //       session.data?.user.email,
+                    //       gameId
+                    //     ),
                     handleState: () => setConfirmBetState(false),
                   }}
                 />
-              ) : null} */}
+              ) : null}
               <div className="flex w-full justify-end space-x-2 pr-4">
                 {session.data?.user.email === "admin@lm.com" ? (
                   <>
@@ -153,75 +150,78 @@ export default function BetTime({
           );
         })}
       </ul>
+      <button className="font-bold text-white" onClick={handleConfirmBet}>
+        Test
+      </button>
     </div>
   );
 }
 // ----------------- WORK IN PROGRESS ----------------------------------------------
-// const ConfirmBet: React.FC = ({ confirmInfo }: ConfirmBetProps) => {
-//   return (
-//     <div
-//       className="relative z-10"
-//       aria-labelledby="modal-title"
-//       role="dialog"
-//       aria-modal="true"
-//     >
-//       <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-//       <div className="fixed inset-0 z-10 overflow-y-auto">
-//         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-//           <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-//             <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-//               <div className="sm:flex sm:items-start">
-//                 <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-//                   <svg
-//                     className="h-6 w-6 text-red-600"
-//                     fill="none"
-//                     viewBox="0 0 24 24"
-//                     stroke-width="1.5"
-//                     stroke="currentColor"
-//                     aria-hidden="true"
-//                   >
-//                     <path
-//                       stroke-linecap="round"
-//                       stroke-linejoin="round"
-//                       d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-//                     />
-//                   </svg>
-//                 </div>
-//                 <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-//                   <h3
-//                     className="text-base font-semibold leading-6 text-gray-900"
-//                     id="modal-title"
-//                   >
-//                     Are you sure you want to choose this time?{" "}
-//                     <span className="font-bold underline">
-//                       {confirmInfo.betTime}
-//                     </span>
-//                   </h3>
-//                 </div>
-//               </div>
-//             </div>
-//             <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-//               <button
-//                 onClick={() => void confirmInfo.handleConfirm()}
-//                 type="button"
-//                 className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-//               >
-//                 Yes, this time
-//               </button>
-//               <button
-//                 onClick={() => void confirmInfo.handleState()}
-//                 type="button"
-//                 className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-//               >
-//                 Close
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+const ConfirmBet: React.FC = ({ confirmInfo }: ConfirmBetProps) => {
+  return (
+    <div
+      className="relative z-10"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+              <div className="sm:flex sm:items-start">
+                <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                  <svg
+                    className="h-6 w-6 text-red-600"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                    />
+                  </svg>
+                </div>
+                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <h3
+                    className="text-base font-semibold leading-6 text-gray-900"
+                    id="modal-title"
+                  >
+                    Are you sure you want to choose this time?{" "}
+                    <span className="font-bold underline">
+                      {/* {confirmInfo.betTime} */}
+                    </span>
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <button
+                onClick={() => void confirmInfo.handleConfirm()}
+                type="button"
+                className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+              >
+                Yes, this time
+              </button>
+              <button
+                onClick={() => void confirmInfo.handleState()}
+                type="button"
+                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 type DeleteResData = {
   success: boolean;
